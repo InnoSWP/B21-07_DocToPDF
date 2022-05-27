b32d298198b17a222ea92fb7a27768c5722b38d2
MasterLogick <31408232+masterlogick@users.noreply.github.com>
2022-05-27 17:58:25 +0300

    secure conversion from leaving files in filesystem

--- app.py
+++ app.py
@@ -1,37 +1,30 @@
+import io
 import os
+import subprocess
+import tempfile
+
 from werkzeug.utils import secure_filename
-from supporting import get_files_names, convert
-
-from flask import Flask, request, jsonify, send_from_directory
-
-from time import time
-
-# from docx2pdf import convert
+from flask import Flask, request, jsonify, send_file
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-@app.route('/files', methods=["GET"])
-def get_files():
-    return jsonify({"files": get_files_names("files")})
-
-
-@app.route('/update', methods=["POST"])
+@app.route('/convertDocx', methods=["POST"])
 def upload_file():
     f = request.files['file']
-    f.save(f'docx/{f.filename}')
-    a = time()
-    convert(f'docx/{f.filename}', f'pdf/')
-    print("Consumed time: ", time() - a)
-    # return jsonify({'message': 'File successfully uploaded'})
-    # os.system(f"open {f.name}.pdf")
-    filename_without_extention = f.filename.split('.')[0]
-    return send_from_directory("pdf", f'{filename_without_extention}.pdf', as_attachment=True)
+    docx = tempfile.NamedTemporaryFile(suffix=".docx", delete=True)
+    try:
+        pdf = open(docx.name[:-5] + ".pdf", "w")
+        pdf.close()
+        f.save(docx)
+        subprocess.run(
+            ["soffice", "--headless", "--convert-to", "pdf", docx.name, "--outdir", os.path.dirname(pdf.name)])
+        pdf = open(docx.name[:-5] + ".pdf", "rb")
+        mem = io.BytesIO(pdf.read())
+        mem.seek(0)
+        return send_file(mem, as_attachment=True, attachment_filename=f.name[:-5] + ".pdf")
+    finally:
+        docx.close()
+        os.remove(docx.name[:-5] + ".pdf")
 
 
 # curl -F "file=@1" -F "file=@2" http://127.0.0.1:5000/upload-multiple
@@ -44,6 +37,7 @@ def upload_multiple():
         print(sec_name)
         file.save(f"files/{sec_name}")
     return jsonify({'message': 'Files successfully uploaded'})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
