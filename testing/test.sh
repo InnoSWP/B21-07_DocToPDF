0e46bebd80acf6965fbcd29ae0e36947b67240c1
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2022-06-23 22:04:06 +0300

    Update test.sh

--- testing/test.sh
+++ testing/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 http_code=$(curl -s -o out.pdf -w '%{http_code}' -F file=@1.docx http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389fa;)
+echo $http_code
 if [[ $http_code -eq 403 ]]; then
     echo "[+] Test for API key"
     exit 0
