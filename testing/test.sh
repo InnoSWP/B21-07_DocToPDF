adc2a9dc281ff7ecce11f9828aa0b8ef352ab8bc
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2022-06-23 21:59:30 +0300

    Update test.sh

--- testing/test.sh
+++ testing/test.sh
@@ -9,7 +9,7 @@ else
 fi
 
 
-http_code=$(curl -s -v -o out.pdf -w '%{http_code}' -F file=@1.docx http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389f;)
+http_code=$(curl -s -v -o out.pdf -w '%{http_code}' -F file=@./testing/1.docx http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389f;)
 if [[ $http_code -eq 200 ]]; then
     echo "[+] Test for sending good file"
     exit 0
