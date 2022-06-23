635b6f96616c0f6b7d3b52d6104e344fed90222f
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2022-06-23 21:30:54 +0300

    Update test.sh

--- testing/test.sh
+++ testing/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 http_code=$(curl -s -v -o out.pdf -w '%{http_code}' -F file=@1.docx http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389f;)
-
+echo ( $http_code == 200 )
 if ( $http_code == 200 ); then
     echo "[+] Test for sending good file"
 	exit 0
