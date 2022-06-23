be0d305ed37e7d71266f046a54ea7af186add450
Anton Nehaev <30959736+NAD777@users.noreply.github.com>
2022-06-23 21:23:48 +0300

    Update test.sh

--- testing/test.sh
+++ testing/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-http_code=$(curl -s -o out.pdf -w '%{http_code}' -F file=@./testing/1.docx http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389f;)
-if [[ $http_code -eq 200 ]]; then
+http_code=$(curl -s -o out.pdf -w '%{http_code}' -F file=@1.docx http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389f;)
+if (( $http_code == 200 )); then
     echo "[+] Test for sending good file"
 	exit 0
 else
