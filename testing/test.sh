28c6011a11223971f005075ed843a6d133a16b14
Anton Nehaev <30959736+NAD777@users.noreply.github.com>
2022-06-23 21:56:37 +0300

    Update test.sh

--- testing/test.sh
+++ testing/test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+http_code=$(curl -s -o out.pdf -w '%{http_code}' -F file=@1.docx http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389fa;)
+if [[ $http_code -eq 403 ]]; then
+    echo "[+] Test for API key"
+    exit 0
+else
+    echo "[-] Test for API key"
+    exit 1
+fi
+
+
 http_code=$(curl -s -v -o out.pdf -w '%{http_code}' -F file=@1.docx http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389f;)
 if [[ $http_code -eq 200 ]]; then
     echo "[+] Test for sending good file"
-	exit 0
+    exit 0
 else
-	echo "[-] Test for sending good file"
-	exit 1
+    echo "[-] Test for sending good file"
+    exit 1
 fi
