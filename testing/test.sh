9bfcab694ef730f83457f1e5da7d6552c5cec0aa
Anton Nehaev <30959736+NAD777@users.noreply.github.com>
2022-06-24 18:21:17 +0300

    Update test.sh

--- testing/test.sh
+++ testing/test.sh
@@ -33,7 +33,7 @@ else
     exit 1
 fi
 
-http_code=$(curl -s -o out.pdf -w '%{http_code}' -F file=@./testing/Ludens.mp3 http://127.0.0.1:5000/api/convertDocx\?apiKey\=ur38r839u2f8ioejfnndh3wifh3892fyh389f;)
+http_code=$(curl -s -o out.pdf -w '%{http_code}' -F file=@./testing/Ludens.mp3 http://127.0.0.1:5000/api/convertDocx\?apiKey=ur38r839u2f8ioejfnndh3wifh3892fyh389f;)
 if [[ $http_code -eq 500 ]]; then
     echo "[+] Test for not docx files"
 else
