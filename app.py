42e5b5300ebb23ff32f0da46562720f441fd8019
NAD777 <30959736+NAD777@users.noreply.github.com>
2022-06-24 17:03:33 +0300

    To have access from network

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ if __name__ == '__main__':
     print(app.config)
     # UPLOAD_EXTENSIONS
     print("Api key:", app.config["apiKey"])
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
