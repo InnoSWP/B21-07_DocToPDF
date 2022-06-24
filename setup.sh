c9251abe1164ab1f268d3a1c8c1862813749f4d9
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2022-06-24 17:08:29 +0300

    fix dependencies

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 echo "Undate apts"
 sudo apt-get update
-echo "Install python3"
-sudo apt-get -y install python3
-echo "Install pip3"
-sudo apt-get -y install python3-pip
+echo "Install dependencies"
+sudo apt-get -y install python3 python3-pip curl libreoffice unzip build-essential libssl-dev libffi-dev python3-dev python3-venv
 echo "Setup virtual environment"
 python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
+curl -o Fonts.zip -L https://github.com/InnoSWP/B21-07_DocToPDF/releases/latest/download/Fonts.zip
+unzip Fonts.zip -d /root/.fonts/
+rm Fonts.zip
