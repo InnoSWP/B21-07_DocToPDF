06b15cdaad66c68d5ef656a3a4c2ec2e6788117a
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2022-07-02 16:43:39 +0300

    fix libreoffice error on high load

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ def check_file_extention(file_name: str) -> bool:
     extention = file_name[file_name.rfind('.'):]
     return extention in allowed_extentions
 
+def infloop(command):
+    d = subprocess.run(command)
+    if d.returncode != 0:
+        infloop(command)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
@@ -25,12 +29,12 @@ def index():
         f = request.files['file']
         docx = tempfile.NamedTemporaryFile(suffix=".docx", delete=True)
         try:
+            print(docx.name)
             pdf = open(docx.name[:-5] + ".pdf", "w")
             pdf.close()
             f.save(docx)
             docx.flush()
-            subprocess.run(
-                ["soffice", "--headless", "--convert-to", "pdf", docx.name, "--outdir", os.path.dirname(pdf.name)])
+            infloop(["soffice", "--headless", "--nolockcheck", "--convert-to", "pdf", docx.name, "--outdir", os.path.dirname(pdf.name)])
             try:
                 pdf = open(docx.name[:-5] + ".pdf", "rb")
             except FileNotFoundError as e:
@@ -39,7 +43,7 @@ def index():
             mem.seek(0)
             if mem.getbuffer().nbytes == 0:
                 return {"message": "Document conversion error"}, 500
-            return send_file(mem, as_attachment=True, attachment_filename=f.filename[:-5] + ".pdf")
+            return send_file(mem, as_attachment=True, download_name=f.filename[:-5] + ".pdf")
         finally:
             docx.close()
             os.remove(docx.name[:-5] + ".pdf")
@@ -65,8 +69,7 @@ def upload_file():
         pdf.close()
         f.save(docx)
         docx.flush()
-        subprocess.run(
-            ["soffice", "--headless", "--convert-to", "pdf", docx.name, "--outdir", os.path.dirname(pdf.name)])
+        infloop(["soffice", "--headless", "--nolockcheck", "--convert-to", "pdf", docx.name, "--outdir", os.path.dirname(pdf.name)])
         try:
             pdf = open(docx.name[:-5] + ".pdf", "rb")
         except FileNotFoundError as e:
@@ -75,35 +78,11 @@ def upload_file():
         mem.seek(0)
         if mem.getbuffer().nbytes == 0:
             return {"message": "Document conversion error"}, 500
-        return send_file(mem, as_attachment=True, attachment_filename=f.filename[:-5] + ".pdf")
+        return send_file(mem, as_attachment=True, download_name=f.filename[:-5] + ".pdf")
     finally:
         docx.close()
         os.remove(docx.name[:-5] + ".pdf")
 
-
-@app.route("/test", methods=["GET", "POST"])
-def test():
-    args = request.args
-    if "apiKey" not in args:
-        return {"message": "Wrong api key"}, 403
-
-    if app.config["apiKey"] == args["apiKey"]:
-        return "Access permitted"
-    else:
-        return "Access denied"
-    return request.args
-
-
-# curl -F "file=@1" -F "file=@2" http://127.0.0.1:5000/upload-multiple
-@app.route('/api/upload-multiple', methods=["POST"])
-def upload_multiple():
-    files = request.files.getlist("file")
-    print(files)
-    for file in files:
-        sec_name = secure_filename(file.filename)
-        print(sec_name)
-        file.save(f"files/{sec_name}")
-    return jsonify({'message': 'Files successfully uploaded'})
 
 with open("config.json") as config_file:
     config_data = json.load(config_file)
